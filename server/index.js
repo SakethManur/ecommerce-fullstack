353ac3a017b10c0b17f153116c5aa6597edcb1a4
Your Name <you@example.com>
2025-11-26 20:20:49 +0530

    Force update server index file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('../db'); 
-const express = require('express');
+;
 
 const app = express();
 app.use(cors());
